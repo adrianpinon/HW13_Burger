9bdec42aac80bd81d1d180a4c285d2a6b9d3efa5
Adrian <70490120+adrianpinon@users.noreply.github.com>
2021-01-10 20:55:53 -0600

    added express npm (oops)

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const express = require('express');
 const PORT = process.env.PORT || 8080; 
 const app = express();
 
-app.use(express.static("..."));
+app.use(express.static("public"));
 
 const exphbs = require("expres-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-const routes = require("...")
+const routes = require("./controllers/burgerController.js")
 
 
+
+app.use(routes);
 
 
 
