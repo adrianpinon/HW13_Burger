130df04b27ef71603952de97a6a0e975dbe1b8c0
Adrian <70490120+adrianpinon@users.noreply.github.com>
2021-01-12 11:06:57 -0600

    finalize

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-let mysql = require("mysql");
+const mysql = require("mysql");
 
-let connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
