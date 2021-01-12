5fae8bac5cf6f38ca8c064ddfa87299d702d29b7
Adrian <70490120+adrianpinon@users.noreply.github.com>
2021-01-12 13:10:22 -0600

    updated connection with heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,23 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "vegetable",
-  database: "burger_db"
-});
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "vegetable",
+//   database: "burger_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
